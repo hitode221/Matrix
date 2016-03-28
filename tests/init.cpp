627d9350bbdb2dda742c4e6e4ef9b97e7d68bde2
hitode221 <marina.dr98@mail.ru>
2016-03-28 16:53:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,1 +1,23 @@
+#include <Matrix.hpp>
+#include <catch.hpp>
+#include <fstream>
 
+SCENARIO("Matrix init", "[init]") {
+
+	GIVEN("The number of rows and columns") {
+
+		auto rows = 3;
+		auto columns = 3;
+
+		WHEN("Create instansce of Matrix") {
+
+			Matrix matrix(rows, columns);
+
+			THEN("The number of rows and columns must be preserved") {
+
+				REQUIRE(matrix.lines() == rows);
+				REQUIRE(matrix.columns() == columns);
+			}
+		}
+	}
+}
